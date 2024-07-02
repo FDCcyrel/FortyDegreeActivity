03f0ff559bc6dc4e2b6a6316b000f53600b6454c
FDCcyrel <fdc.cyrel@gmail.com>
2024-07-02 10:11:12 +0800

    Add files via upload

---
+++ messageboard/app/Test/Fixture/UserFixture.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * User Fixture
+ */
+class UserFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'photo_path' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'email' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'birthday' => array('type' => 'date', 'null' => true, 'default' => null),
+		'gender' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 50, 'unsigned' => false),
+		'hobby' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'password' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'confirm_password' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'date_created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'date_updated' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'last_login_time' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'photo_path' => 'Lorem ipsum dolor sit amet',
+			'email' => 'Lorem ipsum dolor sit amet',
+			'birthday' => '2024-06-24',
+			'gender' => 1,
+			'hobby' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'password' => 'Lorem ipsum dolor sit amet',
+			'confirm_password' => 'Lorem ipsum dolor sit amet',
+			'date_created' => '2024-06-24 13:16:23',
+			'date_updated' => '2024-06-24 13:16:23',
+			'last_login_time' => '2024-06-24 13:16:23'
+		),
+	);
+
+}
